737c05c6297f090c99466e7f0bfd6f5dea6df36d
zombaki <piyush.nath.2272@gmail.com>
2019-03-29 12:59:12 -0500

    2 files modified, 12 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { RootListComponent } from './root-list/root-list.component';
-import { ComponentComponent } from './component/component.component';
-import { ContainerComponent } from './component/container/container.component';
-import { ContaineeComponent } from './component/containee/containee.component';
 import { MatIconModule, MatSidenavModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {DragDropModule} from '@angular/cdk/drag-drop';
@@ -16,7 +12,7 @@ import { TreeElementComponent } from './tree/tree-element/tree-element.component
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, MatIconModule, BrowserAnimationsModule, MatSidenavModule, DragDropModule ],
-  declarations: [ AppComponent, RootListComponent, ComponentComponent, ContainerComponent, ContaineeComponent, TreeComponent, TreeElementComponent ],
+  declarations: [ AppComponent,  TreeComponent, TreeElementComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
